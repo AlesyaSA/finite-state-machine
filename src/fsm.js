5d56ff0754ead9338ba2340b1a72e620c606e4c6
Alesya <saauser77@mail.ru>
2017-02-04 01:19:35 +0300

    finite-state-mashine/feachure:half-made trigger

--- src/fsm.js
+++ src/fsm.js
@@ -3,57 +3,97 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
-
+    constructor(config) {
+        this.config = config;
+        if (config == undefined) {
+            throw new Error();
+        }
+        return this;
+    }
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+
+        return this.config.initial;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+
+        this.state = state;
+        if (
+            (state == 'normal') ||
+            (state == 'busy') ||
+            (state == 'sleeping') ||
+            (state == 'hungry')
+        )
+            this.config.initial = this.state;
+        else {
+            throw new Error();
+        }
+        return this;
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        switch(event){
+          case 'study':
+          this.config.initial= 'busy';
+          break
+          case 'get-tired':
+          this.config.initial='sleeping' ;
+          break
+          case 'get_hungry':
+          this.config.initial='hungry' ;
+          break
+          case 'eat':
+          this.config.initial= 'normal';
+          break
+          case 'get-up':
+          this.config.initial='normal' ;
+          break
+        }
+}
 
-    /**
-     * Resets FSM state to initial.
-     */
-    reset() {}
+/**
+ * Resets FSM state to initial.
+ */
+reset() {}
 
-    /**
-     * Returns an array of states for which there are specified event transition rules.
-     * Returns all states if argument is undefined.
-     * @param event
-     * @returns {Array}
-     */
-    getStates(event) {}
+/**
+ * Returns an array of states for which there are specified event transition rules.
+ * Returns all states if argument is undefined.
+ * @param event
+ * @returns {Array}
+ */
+getStates(event) {}
 
-    /**
-     * Goes back to previous state.
-     * Returns false if undo is not available.
-     * @returns {Boolean}
-     */
-    undo() {}
+/**
+ * Goes back to previous state.
+ * Returns false if undo is not available.
+ * @returns {Boolean}
+ */
+undo() {}
 
-    /**
-     * Goes redo to state.
-     * Returns false if redo is not available.
-     * @returns {Boolean}
-     */
-    redo() {}
+/**
+ * Goes redo to state.
+ * Returns false if redo is not available.
+ * @returns {Boolean}
+ */
+redo() {}
 
-    /**
-     * Clears transition history
-     */
-    clearHistory() {}
+/**
+ * Clears transition history
+ */
+clearHistory() {}
 }
 
 module.exports = FSM;
